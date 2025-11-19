0f5ec77738e68090b13ab9355575df991c7cb3cd
Yang-Yiming <44901560@qq.com>
2025-11-20 00:08:49 +0800

    feat: capacitor android support and github action

---
+++ android/app/src/main/java/com/dozzi/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dozzi.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dozzi.app',
+  appName: 'Dozzi',
+  webDir: 'dist'
+};
+
+export default config;
